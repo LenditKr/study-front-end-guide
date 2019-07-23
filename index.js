539f041819d4a1b4bc0d8f5fc7c6eab6370e0956
Doug <doug@lendit.co.kr>
2019-07-23 10:49:47 +0900

    delete dependencies (#8)
    * delete dependencies
    
    * delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const pkg = require('./package.json');
-
-console.log('These are the dependencies that we use:');
-console.log('  ' + Object.keys(pkg.dependencies).join('\n  '));
-console.log();
-console.log('These are the devDependencies that we use:');
-console.log('  ' + Object.keys(pkg.devDependencies).join('\n  '));
